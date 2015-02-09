c18c699aa3e7f9f01de25e6c6bf931355e1c8b6c
Roman Dvoinev <dvoinev@gmail.com>
2015-02-09 11:04:19 +0300

    Comments cleaned up

--- PRFLR/PRFLR.h
+++ PRFLR/PRFLR.h
@@ -1,9 +1,6 @@
 //
 //  PRFLR.h
-//  Example
-//
-//  Created by Roman on 04/02/15.
-//  Copyright (c) 2015 PRFLR. All rights reserved.
+//  PRFLR
 //
 
 #import <Foundation/Foundation.h>
--- PRFLR/PRFLR.m
+++ PRFLR/PRFLR.m
@@ -1,9 +1,6 @@
 //
 //  PRFLR.m
-//  Example
-//
-//  Created by Roman on 04/02/15.
-//  Copyright (c) 2015 PRFLR. All rights reserved.
+//  PRFLR
 //
 
 #import "PRFLR.h"
